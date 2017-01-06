858f01d84341d91266ece3677fdbf172436f14cb
Felipe F. Diogo <fkdiogo@gmail.com>
2017-01-06 11:43:02 -0200

    adds nodemon
    adds library to reload server as changes are being made

--- javascript-learning-sandbox/orders-app/app.js
+++ javascript-learning-sandbox/orders-app/app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  //res.locals.authenticated = !req.user.anonymous;
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('\n\nHello, world!\n\n');
 });
